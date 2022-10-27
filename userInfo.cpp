ff4d6e2c853b0296e9105f7cf74cb68169c65457
liguyano <kurumiwu@foxmail.com>
2022-10-27 15:00:48 +0800

    add userInfo

---
+++ userInfo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kurum on 10/27/2022.
+//
+
+#include "userInfo.h"
---
+++ userInfo.h
@@ -0,0 +1,14 @@
+//
+// Created by kurum on 10/27/2022.
+//
+
+#ifndef QT_DEMO_USERINFO_H
+#define QT_DEMO_USERINFO_H
+
+
+class userInfo {
+
+};
+
+
+#endif //QT_DEMO_USERINFO_H
